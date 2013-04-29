71c7110d227b4102481ad642929ac576ae7e8544
Ted Rogers <tedrog36@gmail.com>
2013-04-29 14:45:41 -0400

    Initial Commit

---
+++ GitTest2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest2
+//
+//  Created by Ted Rogers on 4/29/13.
+//  Copyright (c) 2013 Ted Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
